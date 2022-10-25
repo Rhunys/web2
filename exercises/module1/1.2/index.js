29b0a7a48c0dc05fc045c3ab02494ae9ef3f2989
Your Name <you@example.com>
2022-10-25 11:39:44 +0200

    fix

--- exercises/module1/1.2/index.js
+++ exercises/module1/1.2/index.js
@@ -1,11 +1,7 @@
 const dateTimeNow = new Date();
 const date = dateTimeNow.toLocaleDateString();
 const time = dateTimeNow.toLocaleTimeString();
+alert(`${date} ${time}: This is the best moment to have a look at this website !`);
+
 console.log(date); // 17/08/2020
 console.log(time); // 13:26:15
-
-function alert() {
-    timeoutID = setTimeout(() => {
-      alert(`${date} ${time}: This is the best moment to have a look at this website !`);
-    }, delayInMiliSeconds);
-  }
