49d9c97342ff61818de1044012c66323367cdf5a
Your Name <you@example.com>
2022-10-25 11:43:27 +0200

    fix

--- exercises/module1/1.2/index.js
+++ exercises/module1/1.2/index.js
@@ -1,6 +1,11 @@
 const dateTimeNow = new Date();
 const date = dateTimeNow.toLocaleDateString();
 const time = dateTimeNow.toLocaleTimeString();
-const alert = alert(`${date} ${time}: This is the best moment to have a look at this website !`);
-alert;
-console.log(alert); // 17/08/2020
+
+function timeAlert() {
+    alert(`${date} ${time}: This is the best moment to have a look at this website !`);
+}
+
+timeAlert();
+
+console.log(timeAlert()); 
