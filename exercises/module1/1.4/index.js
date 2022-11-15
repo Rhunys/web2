f105d66cc0be94528d29f8cb0fd3bb8b98333484
Your Name <you@example.com>
2022-11-15 12:17:12 +0100

    1.4

--- exercises/module1/1.4/index.js
+++ exercises/module1/1.4/index.js
@@ -5,34 +5,34 @@ const greenLight = document.querySelector('#green');
 const btn = document.querySelector("button");
 const clockHolder = document.querySelector("span");
 
-btn1.addEventListener("click", stopOrResumeClock);
+btn.addEventListener("click", stopOrResumeClock);
 
 let myInterval;
 
-redLight();
+redLightOn();
 
-function redLight(){
+function redLightOn(){
     orangeLight.style.backgroundColor = "white";
     redLight.style.backgroundColor = "red";
-    myInterval = setTimeout(orangeLight,2000);
+    myInterval = setTimeout(orangeLightOn1,2000);
 }
 
-function orangeLight1(){
+function orangeLightOn1(){
     redLight.style.backgroundColor = "white";
     orangeLight.style.backgroundColor = "orange";
-    myInterval = setTimeout(greenLight,2000);
+    myInterval = setTimeout(greenLightOn,2000);
 }
 
-function greenLight(){
+function greenLightOn(){
     orangeLight.style.backgroundColor = "white";
     greenLight.style.backgroundColor = "green";
-    myInterval = setTimeout(orangeLight2,2000);
+    myInterval = setTimeout(orangeLightOn2,2000);
 }
 
-function orangeLight2(){
+function orangeLight2On(){
     greenLight.style.backgroundColor = "white";
     orangeLight.style.backgroundColor = "orange";
-    myInterval = setTimeout(redLight,2000);
+    myInterval = setTimeout(redLightOn,2000);
 }
 
 function stopOrResumeClock() {
