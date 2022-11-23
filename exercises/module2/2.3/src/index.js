b9eeb15a20bd738aee7d3ae672f2d343e258daca
GauthierC <gauthier.collard@student.vinci.be>
2022-11-23 12:43:12 +0100

    2.3

--- exercises/module2/2.3/src/index.js
+++ exercises/module2/2.3/src/index.js
@@ -1,2 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './stylesheets/main.css';
+
+
+
+/*
+function createArray(nbLignes,nbColonnes,string){
+
+}
+
+function createHtmlTableAsString(array){
+
+}
+*/
